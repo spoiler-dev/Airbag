e72476645767efca9e5fba8767492ac5a3d4277f
spolier <spolier@icloud.com>
2018-10-26 03:39:05 +0800

    完成3D电气室，添加banner

--- src/views/works/Works.vue
+++ src/views/works/Works.vue
@@ -7,18 +7,19 @@
       <div class="work-card">
         <router-link to="/3DElectricalRoom">
         <div class="card-top">
-          <div class="banner img-up" style="background: url('/img/works/Data.jpg')">
+          <div class="banner img-up" style="background: url('/img/works/Machine.jpg')">
             <div class="banner-label">
               <label>Three.js</label>
               <label>Vue</label>
+              <label>定时刷新</label>
             </div>
           </div>
-          <div class="banner img-mid" style="background: url('img/works/Data.jpg')"></div>
-          <div class="banner img-down" style="background: url('img/works/Data.jpg')"></div>
+          <div class="banner img-mid" style="background: url('img/works/Machine.jpg')"></div>
+          <div class="banner img-down" style="background: url('img/works/Machine.jpg')"></div>
         </div>
         <div class="card-content">
-          <h3 class="card-name">3D 电气室自定义规划系统</h3>
-          <p class="card-desc">原生 Javascript 拖拽平面模块，利用 Three.js 打造的 3D 电气室</p>
+          <h3 class="card-name">3D 电气室实时监控系统</h3>
+          <p class="card-desc">结合 Vue 框架，利用 Three.js 打造的可定时刷新的 3D 电气室</p>
         </div>
         </router-link>
       </div>
@@ -29,6 +30,7 @@
             <div class="banner-label">
               <label>Three.js</label>
               <label>Vue</label>
+              <label>拖拽</label>
             </div>
           </div>
           <div class="banner img-mid" style="background: url('img/works/Data.jpg')"></div>
