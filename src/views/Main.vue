e5a6be76861c1c0e2cf7338a668631cb63de5348
spolier <spolier@icloud.com>
2018-10-31 14:10:19 +0800

    修复bootstrap折叠

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -149,11 +149,11 @@ export default {
       if ($(window).width() < 768) {
         //点击导航链接之后，把导航选项折叠起来
         $("#navbar a").click(function () {
-          $("#navbar").collapse('hide')
+          $("#navbarSupportedContent").collapse('hide')
         });
         //滚动屏幕时，把导航选项折叠起来
         $(window).scroll(function () {
-          $("#navbar").collapse('hide')
+          $("#navbarSupportedContent").collapse('hide')
         })
       }
     })
