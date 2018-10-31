8030cc03675946f7cf096bf192cc139c0f40a50b
spolier <spolier@icloud.com>
2018-10-31 15:03:55 +0800

    收缩栏自动收缩

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -144,13 +144,13 @@ export default {
 
   mounted () {
     $('.dropdown-toggle').dropdown()
+    //点击导航链接之后，把导航选项折叠起来
+    $(".nav-collapse").click(function () {
+      $("#navbarSupportedContent").collapse('hide')
+    })
     $(window).scroll(function () {
       //小屏幕下的导航条折叠
       if ($(window).width() < 768) {
-        //点击导航链接之后，把导航选项折叠起来
-        $(".nav-collapse").click(function () {
-          $("#navbarSupportedContent").collapse('hide')
-        })
         //滚动屏幕时，把导航选项折叠起来
         $(window).scroll(function () {
           $("#navbarSupportedContent").collapse('hide')
