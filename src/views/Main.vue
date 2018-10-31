c3b65b656a2bd59215684ac1d629828b1348c6c0
spolier <spolier@icloud.com>
2018-10-31 13:06:12 +0800

    移动端滚动屏幕，导航栏折叠

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -144,6 +144,19 @@ export default {
 
   mounted () {
     $('.dropdown-toggle').dropdown()
+    $(window).scroll(function () {
+      //小屏幕下的导航条折叠
+      if ($(window).width() < 768) {
+        //点击导航链接之后，把导航选项折叠起来
+        $("#navbar a").click(function () {
+          $("#navbar").collapse('hide')
+        });
+        //滚动屏幕时，把导航选项折叠起来
+        $(window).scroll(function () {
+          $("#navbar").collapse('hide')
+        })
+      }
+    })
     window.onscroll = () => {
       let top = document.body.scrollTop
       if ($('html').scrollTop() > ($('#navbar').height() + 50) || $('body').scrollTop() > ($('#navbar').height() + 50)) {
